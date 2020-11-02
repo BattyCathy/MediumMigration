f1f6cd0a81a952bb3f53a0a07b5d52e4ea7b71f8
BattyCathy <austinroach@gmail.com>
2020-11-02 15:58:08 -0800

    Square only oscillator need to implement real time sliders

--- MediumMigration/ContentView.swift
+++ MediumMigration/ContentView.swift
@@ -12,17 +12,19 @@ struct ContentView: View {
     @State public var freqValue: Float = 50
     @State public var ampValue: Float = 0.5
     @State public var isTicking: Bool = false
-    
-    
+  
+
     var body: some View {
         VStack() {
-        Slider(value: $freqValue, in: 1 ... 10000)
+        Slider(value: $freqValue, in: 1 ... 1000)
         Slider(value: $ampValue, in: 0 ... 1)
         Toggle(isOn: $isTicking) {
             Text("Play/Pause")
         }
         Button(action: {
             playPause()
+            setSynthParametersFrom()
+            setAsSquare()
         }) {
             Text("Start")
         }
@@ -32,32 +34,32 @@ struct ContentView: View {
         }
        
     }
+    
+    func setAsSquare() {
+        Synth.shared.setWaveformTo(Oscillator.square)
+    }
+    
     func setSynthParametersFrom() {
         Oscillator.amplitude = Float(ampValue)
         Synth.shared.frequency = Float(freqValue)
     }
    
-    func sliderUpdate() {
-        if Oscillator.amplitude != Float(ampValue) ||
-            Synth.shared.frequency != Float(freqValue) {
-            setSynthParametersFrom()
-        }
-    }
-    
+   
     
     func playPause() {
         if isTicking == true {
-        setSynthParametersFrom()
         setPlaybackStateTo(true)
         } else if isTicking == false {
-            setSynthParametersFrom()
             setPlaybackStateTo(false)
         }
     }
+    
+    
     func setPlaybackStateTo(_ state: Bool) {
         Synth.shared.volume = state ? 0.5 : 0
         if !state { Synth.shared.frequency = 0 }
     }
+    
 }
   
 struct ContentView_Previews: PreviewProvider {
