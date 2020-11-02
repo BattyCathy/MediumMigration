9a9736bfbdd669c5332dcb83c540ea51a387256c
BattyCathy <austinroach@gmail.com>
2020-11-02 13:18:26 -0800

    Initial Commit

---
+++ MediumMigration/MediumMigrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediumMigrationApp.swift
+//  MediumMigration
+//
+//  Created by Austin Roach on 11/2/20.
+//
+
+import SwiftUI
+
+@main
+struct MediumMigrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
